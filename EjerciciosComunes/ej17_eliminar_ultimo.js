97cb934efcb94e3bf021e4e5518145671c53d8f4
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-05 01:50:17 -0300

    Ej17 - Eliminar dos últimos elementos de un array

--- EjerciciosComunes/ej17_eliminar_ultimo.js
+++ EjerciciosComunes/ej17_eliminar_ultimo.js
@@ -8,8 +8,11 @@ console.log("Eliminar los dos últimos elementos de un array");
 // Creo un array con varios elementos
 let miArray = [1, 2, 3, 4, 5, 6];
 
+// Imprimo el array inicial
+console.log("Array inicial: ", miArray);
+
 // Elimino los dos últimos elementos del array
 miArray.splice(-2, 2);
 
 // Imprimo el array resultante
-console.log("Array resultante:", miArray);
+console.log("Array resultante: ", miArray);
