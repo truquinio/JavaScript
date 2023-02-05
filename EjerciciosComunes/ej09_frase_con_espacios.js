2488abb8b10cfa24daf528ac2ddd467feaa30dcc
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-05 01:14:24 -0300

    Ej09 - Espacio entre cada letra de frase ingresada. Ej: H o l a

---
+++ EjerciciosComunes/ej09_frase_con_espacios.js
@@ -0,0 +1,23 @@
+/*
+Realizar un programa que pida una frase y el programa deberá mostrar la frase con un
+espacio entre cada letra. La frase se mostrará así: H o l a. Nota: recordar el
+funcionamiento de la función Substring().
+*/
+
+alert("Espacio entre cada letra de frase ingresada. Ej: H o l a")
+
+// Pido al usuario que ingrese una frase
+var frase = prompt("Ingrese una frase:");
+
+// Inicializo la variable que guardará la frase con espacios entre cada letra
+var fraseEspaciada = "";
+
+// Recorro cada letra de la frase
+for (var i = 0; i < frase.length; i++) {
+
+  // Agrego el caracter actual a la frase espaciada, seguido de un espacio
+  fraseEspaciada += frase.charAt(i) + " ";
+}
+
+// Muestro la frase espaciada
+console.log(fraseEspaciada);
