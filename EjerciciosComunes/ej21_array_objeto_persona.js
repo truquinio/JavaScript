e06abf6b415099e074b4eca9c6de92f1c27a5f1c
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-05 02:43:35 -0300

    Array con attr de objeto persona

---
+++ EjerciciosComunes/ej21_array_objeto_persona.js
@@ -0,0 +1,26 @@
+/*
+Escribir un programa para obtener un array de las propiedades de un objeto Persona.
+Las propiedades son nombre, edad, sexo ('H' hombre, 'M' mujer, 'O' otro), peso y altura.
+*/
+
+console.log("Array con attr de objeto persona")
+
+// Defino el objeto persona
+let persona = {
+  nombre: "Lalo",
+  edad: 30,
+  sexo: "H",
+  peso: 80,
+  altura: 1.75
+};
+
+// Creo una variable para almacenar las propiedades
+let propiedades = [];
+
+// Recorro las propiedades del objeto persona y las guardo en el array propiedades
+for (propiedad in persona) {
+  propiedades.push(propiedad);
+}
+
+// Muestro el resultado en consola
+console.log(propiedades);
