50742d26c67ff8e950a767904148b5143410fdae
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-05 01:30:44 -0300

    Ej14 - Crear y cargar objeto libro, mostrar Attr

---
+++ EjerciciosComunes/ej14_objeto_libro.js
@@ -0,0 +1,38 @@
+/*
+Crear un objeto libro que contenga las siguientes propiedades: ISBN, Título, Autor,
+Número de páginas. Crear un método para cargar un libro pidiendo los datos al usuario
+y luego informar mediante otro método el número de ISBN, el título, el autor del libro y el
+numero de páginas.
+*/
+
+alert("Crear y cargar objeto libro, mostrar Attr");
+
+// Creo un objeto libro con las propiedades especificadas
+const libro = {
+  ISBN: "",
+  titulo: "",
+  autor: "",
+  numPaginas: 0,
+
+  // Creo un método para cargar un libro
+  cargarLibro: function () {
+    this.ISBN = prompt("Ingrese el ISBN");
+    this.titulo = prompt("Ingrese el título");
+    this.autor = prompt("Ingrese el autor");
+    this.numPaginas = parseInt(prompt("Ingrese el número de páginas"));
+  },
+
+  // Creo un método para informar los datos del libro
+  informarDatos: function () {
+    console.log(`ISBN: ${this.ISBN}`);
+    console.log(`Título: ${this.titulo}`);
+    console.log(`Autor: ${this.autor}`);
+    console.log(`Número de páginas: ${this.numPaginas}`);
+  }
+};
+
+// Llamo al método cargarLibro para que el usuario ingrese los datos del libro
+libro.cargarLibro();
+
+// Llamo al método informarDatos para que se muestren los datos del libro
+libro.informarDatos();
