9432b27c12688d34a66c00049797d81e756dc411
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-07 12:28:55 -0300

    Ej08 - Calcular max, min y promedio de n° ingresados

--- EjerciciosComunes/ej08_max_min_promedio.js
+++ EjerciciosComunes/ej08_max_min_promedio.js
@@ -7,38 +7,38 @@ todos ellos.
 alert("Mostrar el máximo, el mínimo, y el promedio de todos los n° ingresados");
 
 // Inicializo las variables que guardarán el mínimo, máximo y la suma total de los números ingresados
-var min = Number.POSITIVE_INFINITY;
-var max = Number.NEGATIVE_INFINITY;
-var sum = 0;
+let min = Number.POSITIVE_INFINITY;
+let max = Number.NEGATIVE_INFINITY;
+let total = 0;
 
 // Inicializo la cantidad de números ingresados
-var count = 0;
+let contador = 0;
 
 // Pido al usuario que ingrese un número
-var num = parseInt(prompt("Ingrese un número entero (0 para finalizar):"));
+let num = parseInt(prompt("Ingrese un número entero (0 para finalizar):"));
 
 // Repito el proceso mientras el usuario no haya ingresado 0
 while (num !== 0) {
 
   // Incremento la cantidad de números ingresados
-  count++;
+  contador++;
 
   // Actualizo el mínimo y el máximo si es necesario
   min = Math.min(min, num);
   max = Math.max(max, num);
 
-  // Sumo el número al total
-  sum += num;
+  // sumo el número al total
+  total += num;
 
   // Pido al usuario que ingrese otro número
   num = parseInt(prompt("Ingrese un número entero (0 para finalizar):"));
 }
 
 // Muestro el mínimo, máximo y promedio
-if (count > 0) {
+if (contador > 0) {
   console.log("Mínimo: " + min);
   console.log("Máximo: " + max);
-  console.log("Promedio: " + (sum / count));
+  console.log("Promedio: " + (total / contador));
   
 } else {
   console.log("No se ingresaron números.");
