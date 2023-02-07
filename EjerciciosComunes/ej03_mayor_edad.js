7642aa493aa7cdd0a6409de68f769a9058766252
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-07 12:09:33 -0300

    Ej03 - validar si usuario es mayor de edad

--- EjerciciosComunes/ej03_mayor_edad.js
+++ EjerciciosComunes/ej03_mayor_edad.js
@@ -5,10 +5,18 @@ Escriba un programa en donde se pida la edad del usuario. Si el usuario es mayor
 alert("Validar si es mayor de edad")
 
 // Solicita la edad del usuario
-var edad = parseInt(prompt("Ingrese su edad"));
+let edad = parseInt(prompt("Ingrese su edad"));
 
-// Verifica si el usuario es mayor de edad
+// Verifica si el usuario es mayor de edad y muestra el msj (op. ternario)
+console.log(edad >= 18 ? "Usted es mayor" : "Usted es menor");
+
+
+
+
+// OTRA FORMA verifica si el usuario es mayor de edad
 if (edad >= 18) {
+
   // Si el usuario es mayor de edad, muestra el msj
+  
   console.log("Usted es mayor de edad");
 }
