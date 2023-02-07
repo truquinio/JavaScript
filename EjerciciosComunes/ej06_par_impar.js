7a64d5693f5ac98b6f1c8e218013726bda610899
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-07 12:25:40 -0300

    Ej06 - Mostrar si N° entero es par o impar

--- EjerciciosComunes/ej06_par_impar.js
+++ EjerciciosComunes/ej06_par_impar.js
@@ -6,19 +6,28 @@ En caso de que el valor ingresado sea 0, se debe mostrar “el número no es par
 alert("Calcular num par o impar")
 
 // Declaro una variable para almacenar el número ingresado
-var num = prompt("Ingrese un número entero:");
+let num = prompt("Ingrese un número entero:");
 
 // Verificamos si el número es 0
 if (num == 0) {
-  
+
   // Si es 0, muestro un mensaje que dice que no es ni par ni impar
   console.log("El número no es par ni impar");
 } else {
-  // Si no es 0, verificamos si es par o impar
+
+  // Si el residuo de la división por 2 es 0, entonces es par, sino impar
+  console.log(num % 2 == 0 ? "El número es par" : "El número es impar");
+
+
+  
+  // OTRA FORMA: Si no es 0, verificamos si es par o impar
   if (num % 2 == 0) {
+
     // Si el residuo de la división por 2 es 0, entonces es par
     console.log("El número es par");
+
   } else {
+
     // Si no es 0, entonces es impar
     console.log("El número es impar");
   }
