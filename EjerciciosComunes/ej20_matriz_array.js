0bdc6f4b6facb2d72c039060769ea898fa3d8b70
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-07 13:24:57 -0300

    Ej20 - De matriz a array sumando 3 a c/ elemento

--- EjerciciosComunes/ej20_matriz_array.js
+++ EjerciciosComunes/ej20_matriz_array.js
@@ -3,10 +3,11 @@ Realizar un programa que obtenga la siguiente matriz [[3], [6], [9], [12], [15]]
 muestre el siguiente array [6, 9, 12, 15, 18].
 */
 
-console.log("Pasar de matriz a array sumando 3 a cada elemento")
+console.log("De matriz a array sumando 3 a cada elemento");
 
 // Defino e imprimo la matriz original
 let matriz = [[3], [6], [9], [12], [15]];
+
 console.log("Matriz original: ", matriz);
 
 // Creo una variable para almacenar el resultado
