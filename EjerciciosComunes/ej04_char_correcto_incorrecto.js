4f9e93beffac16402895d39460521d4e0fe80a82
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-07 12:22:46 -0300

    Ej04 - Validar char Correcto o Incorrecto

--- EjerciciosComunes/ej04_char_correcto_incorrecto.js
+++ EjerciciosComunes/ej04_char_correcto_incorrecto.js
@@ -9,7 +9,11 @@ alert("validar caracter CORRECTO o INCORRECTO")
 // Pido al usuario que ingrese un carácter
 let char = prompt("Ingrese un carácter (S o N)").toUpperCase();
 
-// Verifico si el carácter ingresado es 'S' o 'N'
+// Verifico si el carácter ingresado es 'S' o 'N' e imprimo msj según corresponda
+console.log(char == 'S' | char == 'N' ? "CORRECTO" : "INCORRECTO");
+
+
+// OTRA FORMA: Verifico si el carácter ingresado es 'S' o 'N'
 if (char === 'S' || char === 'N') {
 
   // Imprimo un mensaje de que la entrada es correcta
