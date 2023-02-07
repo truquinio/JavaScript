70264f1b8259e3ae65fb5b079d52079474e92e2e
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-07 12:24:25 -0300

    Ej05 - Operaciones aritméticas básicas (+ - * /)

--- EjerciciosComunes/ej05_op_math.js
+++ EjerciciosComunes/ej05_op_math.js
@@ -9,15 +9,16 @@ o ‘m’ para la multiplicación y ‘D’ o ‘d’ para la división.
 alert("Operaciones aritméticas básicas (+ - * /)")
 
 // Solicita al usuario que ingrese los dos valores numéricos enteros
-var num1 = parseInt(prompt("Ingrese el primer número"));
-var num2 = parseInt(prompt("Ingrese el segundo número"));
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
 
 // Solicita al usuario que especifique la operación que desea realizar
-var operacion = prompt("Ingrese la operación que desea realizar (S/R/M/D)").toLowerCase();
+let opcion = prompt("Ingrese la operación que desea realizar (S/R/M/D)").toLowerCase();
+
+let resultado;
 
 // Realiza la operación según la opción elegida por el usuario
-var resultado;
-switch (operacion) {
+switch (opcion) {
   case "s":
     resultado = num1 + num2;
     break;
