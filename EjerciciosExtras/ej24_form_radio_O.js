5f69c62be84bbc4afe42b1f18371e129c0c65345
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-08 21:42:33 -0300

    Ej24 - Calcular radio de un círculo mediante formulario

--- EjerciciosExtras/ej24_form_radio_O.js
+++ EjerciciosExtras/ej24_form_radio_O.js
@@ -4,10 +4,10 @@
 window.calcularRadio = function calcularRadio() {
 
 // Obtengo el valor del diámetro ingresado por el usuario
-var diametro = document.getElementById("diametro").value;
+let diametro = document.getElementById("diametro").value;
 
 // Calculo el radio dividiendo el diámetro entre 2
-var radio = diametro / 2;
+let radio = diametro / 2;
 
 // Muestro el resultado en el elemento div
 document.getElementById("resultado").innerHTML = "El radio es: " + radio;
